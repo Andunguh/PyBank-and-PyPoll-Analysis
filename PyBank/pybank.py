2b88a17d67901733e80de841ae9beb3c58432320
Andunguh <anthonynndungu@gmail.com>
2021-10-03 14:41:25 -0400

    finish pybank

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -1,23 +1,33 @@
 import csv
+months_list=[]
+values_list=[] 
+differences_list=[]
 
 with open("Resources/budget_data.csv","r") as budget_data:
     csvreader = csv.reader(budget_data)
-    total_months=0
+    
     column_names = next(csvreader)   
     for row in csvreader: 
-        print(row)
-        total_months=total_months+1 
+        
+        months_list.append(row[0])
+        values_list.append(int(row[1]))
 
+for index ,value in enumerate(values_list):
+    if index ==0:
+        differences_list.append(0)
+    else:
+        differences_list.append(value-values_list[index-1])
 
 financial_analysis=f"""
 Financial Analysis
 ----------------------------
-Total Months: {total_months}
-Total: $38382578
-Average  Change: $-2315.12
-Greatest Increase in Profits: Feb-2012 ($1926159)
-Greatest Decrease in Profits: Sep-2013 ($-2196167)
+Total Months: {len(months_list)}
+Total: ${sum(values_list)}
+Average  Change: ${round(sum(differences_list)/len(differences_list),3)}
+Greatest Increase in Profits: {months_list[differences_list.index(max(differences_list))]} (${max(differences_list)})
+Greatest Decrease in Profits: {months_list[differences_list.index(min(differences_list))]} (${min(differences_list)})
 """
+
 print (financial_analysis)
 
 with open("analysis/budget_analysis.txt","w") as budget_analysis:
